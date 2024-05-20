1c9371eb0e9f267da1ad074fb730e9998ee7a5e1
eurdream98 <k01054792361@gmail.com>
2024-05-20 12:05:24 +0900

    commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:latest
-LABEL authors="eurdr"
-
-ENTRYPOINT ["top", "-b"]
+FROM openjdk:17
+ARG JAR_FILE=build/libs/*.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,2 +1,13 @@
-package com.example.demo;public class Controller {
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/test2")
+    public ResponseEntity<String> test(){
+        return ResponseEntity.ok("안녕하세요.");
+    }
 }
