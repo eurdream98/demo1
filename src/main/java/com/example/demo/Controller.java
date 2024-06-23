4ca153d21848283dbc837064bbf959258e5dada2
eurdream98 <k01054792361@gmail.com>
2024-06-23 14:04:39 +0900

    commit

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/test2")
     public ResponseEntity<String> test(){
+
         return ResponseEntity.ok("안녕하세요.");
     }
 
