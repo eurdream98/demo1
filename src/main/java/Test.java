5ceec32158793d8fc38709f924da6947a510fc08
eurdream98 <k01054792361@gmail.com>
2024-07-07 13:41:02 +0900

    commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        
         System.out.println(a);
-
     }
 }
